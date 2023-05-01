9328502e04e0bec26b6a4ed2b1a94d4570a82dd0
Viktorzee <monsieurviktor@yahoo.com>
2023-05-01 02:05:29 +0100

    set up config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature(),
+    ConfigModule.forRoot({
+      load: [config]
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
