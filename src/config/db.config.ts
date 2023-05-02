79c4e2181f4d4e2244405fca18620a06b34ce166
Viktorzee <monsieurviktor@yahoo.com>
2023-05-02 12:14:31 +0100

    moved config to individual files

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import {TypeOrmModuleOptions} from '@nestjs/typeorm'
 
-export default registerAs<TypeOrmModule>('database', () => ({
+export default registerAs<TypeOrmModuleOptions>('database', () => ({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    port:  parseInt(process.env.DATABASE_PORT || '5432', 10),
 }))
