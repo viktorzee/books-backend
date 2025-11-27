b6eb739914e2e4fa8dcd708077b9bb8a5698eca7
VictorOpara <oparavictor.dev1@gmail.com>
2025-11-27 16:39:58 +0100

    fixed module issues

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -4,9 +4,15 @@ import { ReviewService } from "./review.service";
 import { ReviewController } from "./review.controller";
 import { Review } from "./entities/review.entity";
 import { Book } from "src/book/entities/book.entity";
+import { AuthModule } from "src/auth/auth.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, Book])],
+  imports: [
+    TypeOrmModule.forFeature([Review, Book]),
+    AuthModule,
+    JwtModule,
+  ],
   controllers: [ReviewController],
   providers: [ReviewService],
   exports: [ReviewService],
